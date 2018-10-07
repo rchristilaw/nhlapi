c1082cc209ca0fbf9f68596a2ff7fd55cdc29618
Ryan Christilaw <ryan.christilaw@gmail.com>
2018-10-07 08:58:39 -0400

    Add packaging support

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='nhlapi',
+    version='0.0.1',
+    description='an API used to parse JSON data from the NHL website',
+    license='MIT',
+    packages=['nhlapi'],
+    author='Ryan Christilaw',
+    author_email='ryan.christilaw@gmail.com',
+    keywords=['nhl scores'],
+    url='https://github.com/rchristilaw'
+)
