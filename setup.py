fb403705d3a13833d0bdca3e7be1b1f226889023
Ryan Christilaw <ryan.christilaw@gmail.com>
2018-10-14 13:41:16 -0400

    Update python setup file

--- setup.py
+++ setup.py
@@ -5,9 +5,14 @@ setup(
     version='0.0.1',
     description='an API used to parse JSON data from the NHL website',
     license='MIT',
-    packages=['nhlapi'],
     author='Ryan Christilaw',
     author_email='ryan.christilaw@gmail.com',
     keywords=['nhl scores'],
-    url='https://github.com/rchristilaw'
+    url='https://github.com/rchristilaw/nhlapi',
+    packages=['nhlapi'],
+    install_requires=[
+        'requests',
+        'python-dateutil',
+        'pytz'
+    ]
 )
